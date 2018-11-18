68dbab7d8bf9b7857ce632cf2b5654728c0f43b5
Ashwin Nishad <ashwinnnishad@gmail.com>
2018-11-18 09:55:55 -0600

    converted to stateless functional components

--- src/app.js
+++ src/app.js
@@ -56,57 +56,51 @@ class IndecisionApp extends React.Component {
     );
   }
 }
-class Header extends React.Component {
-  render() { // this method is required whenever extending from React.Component
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
 }
 
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button
-         onClick={this.props.handlePick}
-         disabled={!this.props.hasOptions}
-         >
-          What should I do?
-        </button>
-      </div>
-    );
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button
+       onClick={props.handlePick}
+       disabled={!props.hasOptions}
+       >
+        What should I do?
+      </button>
+    </div>
+  );
 }
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}> Remove all </button>
-        {
-          this.props.options.map((option) => {
-            return <Option key={option} optionText={option}/>
-          })
-        }
-      </div>
-    );
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}> Remove all </button>
+      {
+        props.options.map((option) => {
+          return <Option key={option} optionText={option}/>
+        })
+      }
+    </div>
+  );
 }
 
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-      Option: {this.props.optionText}
-      </div>
+const Option = (props) => {
+  return (
+    <div>
+      Option: {props.optionText}
+    </div>
 
-    );
-  }
+  );
 }
+
 class AddOption extends React.Component {
   constructor(props) {
     super(props);
